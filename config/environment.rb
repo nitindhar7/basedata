a32a924e743f0cfb49479031f2467c452941d079
Nitin Dhar <nitindhar7@yahoo.com>
2011-03-23 17:58:43 -0400

    unpacked rack to project

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "simple-rss"
   config.gem 'will_paginate', :version => '~> 2.3.15', :source => 'http://gemcutter.org'
+  config.gem "rack", :version => "1.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
